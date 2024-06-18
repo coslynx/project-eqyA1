dec4d2a005823ccb44b95550334fc8dd854ae7d4
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-18 10:47:55 +0530

    Added: `discord-bot-project/src/commands/history.js`

---
+++ discord-bot-project/src/commands/history.js
@@ -0,0 +1,9 @@
+const historyCommand = {
+  name: 'history',
+  description: 'Display the viewing history of the user',
+  execute(message, args) {
+    // Logic to display user's viewing history
+  }
+};
+
+module.exports = historyCommand;
